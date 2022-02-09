bd70cec946df4a4be30332c8c7fec13bac6bb3e3
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-02-09 22:40:46 +0530

    mac

---
+++ Asynchrous_programming/record.js
@@ -0,0 +1,29 @@
+console.log(1)
+
+console.log(2)
+console.log(3)
+
+function test(){
+    console.log("vishnu")
+}
+
+
+
+// for(var i = 0;i<1000000000;i++){
+
+// }
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+
+// if we want to run a function after some Time
+// then we have to use following syntax  
+
+setTimeout(test,5000)
+
+
+
+
+
+
