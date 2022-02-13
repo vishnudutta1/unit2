7574abd766507362397e31ea05161645d21e9523
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-02-13 23:17:20 +0530

    mac

---
+++ constructor/learning.js
@@ -0,0 +1,98 @@
+
+ //constructor function 
+
+
+//  var person1 ={
+//      FirstNmae : "Vishnu",
+//      lastNmae : "Dutta",
+//      age : 27
+//  }
+
+//  var person2 ={
+//     FirstNmae : "Bittu",
+//     lastNmae : "Dutta",
+//     age : 26
+// }
+
+
+function Constructo(f,l,a){
+    this.firstName = f,
+    this.lastName = l,
+    this.age =a
+}
+
+var person1 = new Constructo("vishnu","dutta",28)
+
+person1.schoolName = "Kid care"
+
+var person2 = new Constructo("bittu","dutta",27)
+
+console.log(person1)
+
+console.log(person2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  function myname(n,a,i){
+//      this.name = n;
+//      this.age = a;
+//      this.id  = i
+
+//  }
+
+//  var person1 = new myname("dhiraj",25,6)
+//  var person2 = new myname("anand",26,3)
+ 
+ 
+
+
+//  person1.schoolName = "Kid Care"
+
+//  console.log(person1)
+
+
+
+
+ //constructor
+ //apply,call, bind,
+
+
+
